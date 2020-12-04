84f3b1c1e2b8f5e1f36eb8636c66f7e2b7b164d4
direwolf6 <ryanc45@tcd.ie>
2020-12-04 20:30:20 +0000

    initial commit

---
+++ Sodoku Solver/Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sodoku_Solver
+{
+    class Program
+    {
+        public static Sodoku puzzle;
+        public static List<int>[,] possibilities = new List<int>[9, 9];
+
+        static void Main(string[] args)
+        {
+            int[,] start = {
+                {0,0,7,2,8,0,0,0,1},
+                {0,2,0,0,4,0,5,0,0},
+                {5,0,8,0,0,0,0,0,0},
+                {0,0,0,1,7,2,0,0,8},
+                {2,0,0,0,0,0,0,0,4},
+                {6,0,0,4,5,8,0,0,0},
+                {0,0,0,0,0,0,3,0,5},
+                {0,0,4,0,1,0,0,6,0},
+                {8,0,0,0,2,9,1,0,0},
+            };
+
+            puzzle = new Sodoku(start);
+
+            Print(puzzle);
+
+            while (!IsDone())
+            {
+                LowHangingFruit();
+
+                Possibilities();
+
+                //
+                Print(puzzle);
+            }
+        }
+
+        public static void Possibilities()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (possibilities[i, j] != null)
+                    {
+                        foreach(int possibility in possibilities[i, j])
+                        {
+                            if (OnlyPossibleOnce(GetPossibilitiesRow(i), possibility)
+                                || OnlyPossibleOnce(GetPossibilitiesColumn(j), possibility)
+                                || OnlyPossibleOnce(GetPossibilitiesBlock(puzzle.GetBlockNumber(i, j)), possibility))
+                            {
+                                puzzle.Put(i, j, possibility);
+                                Print(puzzle);
+                                possibilities[i, j] = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool IsDone()
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if(puzzle.board[i,j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool OnlyPossibleOnce(List<int>[] group, int value)
+        {
+            int count = 0;
+            foreach(List<int> cellGroup in group)
+            {
+                if(cellGroup != null)
+                {
+                    foreach (int possibility in cellGroup)
+                    {
+                        if (possibility == value)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return (count == 1);
+        }
+
+        public static List<int>[] GetPossibilitiesRow(int i)
+        {
+            return Enumerable.Range(0, 9)
+                    .Select(x => possibilities[i, x])
+                    .ToArray();
+        }
+
+        public static List<int>[] GetPossibilitiesColumn(int j)
+        {
+            return Enumerable.Range(0, 9)
+                    .Select(x => possibilities[x, j])
+                    .ToArray();
+        }
+
+        public static List<int>[] GetPossibilitiesBlock(int blockNumber)
+        {
+            List<int>[] block = new List<int>[9];
+
+            if (blockNumber == 0)
+            {
+                block[0] = possibilities[0, 0];
+                block[1] = possibilities[0, 1];
+                block[2] = possibilities[0, 2];
+                block[3] = possibilities[1, 0];
+                block[4] = possibilities[1, 1];
+                block[5] = possibilities[1, 2];
+                block[6] = possibilities[2, 0];
+                block[7] = possibilities[2, 1];
+                block[8] = possibilities[2, 2];
+            }
+            else if (blockNumber == 1)
+            {
+                block[0] = possibilities[0, 3];
+                block[1] = possibilities[0, 4];
+                block[2] = possibilities[0, 5];
+                block[3] = possibilities[1, 3];
+                block[4] = possibilities[1, 4];
+                block[5] = possibilities[1, 5];
+                block[6] = possibilities[2, 3];
+                block[7] = possibilities[2, 4];
+                block[8] = possibilities[2, 5];
+            }
+            else if (blockNumber == 2)
+            {
+                block[0] = possibilities[0, 6];
+                block[1] = possibilities[0, 7];
+                block[2] = possibilities[0, 8];
+                block[3] = possibilities[1, 6];
+                block[4] = possibilities[1, 7];
+                block[5] = possibilities[1, 8];
+                block[6] = possibilities[2, 6];
+                block[7] = possibilities[2, 7];
+                block[8] = possibilities[2, 8];
+            }
+
+            else if (blockNumber == 3)
+            {
+                block[0] = possibilities[3, 0];
+                block[1] = possibilities[3, 1];
+                block[2] = possibilities[3, 2];
+                block[3] = possibilities[4, 0];
+                block[4] = possibilities[4, 1];
+                block[5] = possibilities[4, 2];
+                block[6] = possibilities[5, 0];
+                block[7] = possibilities[5, 1];
+                block[8] = possibilities[5, 2];
+            }
+            else if (blockNumber == 4)
+            {
+                block[0] = possibilities[3, 3];
+                block[1] = possibilities[3, 4];
+                block[2] = possibilities[3, 5];
+                block[3] = possibilities[4, 3];
+                block[4] = possibilities[4, 4];
+                block[5] = possibilities[4, 5];
+                block[6] = possibilities[5, 3];
+                block[7] = possibilities[5, 4];
+                block[8] = possibilities[5, 5];
+            }
+            else if (blockNumber == 5)
+            {
+                block[0] = possibilities[3, 6];
+                block[1] = possibilities[3, 7];
+                block[2] = possibilities[3, 8];
+                block[3] = possibilities[4, 6];
+                block[4] = possibilities[4, 7];
+                block[5] = possibilities[4, 8];
+                block[6] = possibilities[5, 6];
+                block[7] = possibilities[5, 7];
+                block[8] = possibilities[5, 8];
+            }
+
+            else if (blockNumber == 6)
+            {
+                block[0] = possibilities[6, 0];
+                block[1] = possibilities[6, 1];
+                block[2] = possibilities[6, 2];
+                block[3] = possibilities[7, 0];
+                block[4] = possibilities[7, 1];
+                block[5] = possibilities[7, 2];
+                block[6] = possibilities[8, 0];
+                block[7] = possibilities[8, 1];
+                block[8] = possibilities[8, 2];
+            }
+            else if (blockNumber == 7)
+            {
+                block[0] = possibilities[6, 3];
+                block[1] = possibilities[6, 4];
+                block[2] = possibilities[6, 5];
+                block[3] = possibilities[7, 3];
+                block[4] = possibilities[7, 4];
+                block[5] = possibilities[7, 5];
+                block[6] = possibilities[8, 3];
+                block[7] = possibilities[8, 4];
+                block[8] = possibilities[8, 5];
+            }
+            else if (blockNumber == 8)
+            {
+                block[0] = possibilities[6, 6];
+                block[1] = possibilities[6, 7];
+                block[2] = possibilities[6, 8];
+                block[3] = possibilities[7, 6];
+                block[4] = possibilities[7, 7];
+                block[5] = possibilities[7, 8];
+                block[6] = possibilities[8, 6];
+                block[7] = possibilities[8, 7];
+                block[8] = possibilities[8, 8];
+            }
+
+            return block;
+        }
+
+        public static void LowHangingFruit()
+        {
+            bool hasChanged = true;
+
+            while (hasChanged)
+            {
+                hasChanged = false;
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (puzzle.Get(i, j) == 0)
+                        {
+                            List<int> missingNUmbers = puzzle.GetNumbersMissingFromEveryDemension(i, j);
+
+                            if (missingNUmbers.Count == 1)
+                            {
+                                puzzle.Put(i, j, missingNUmbers[0]);
+                                Print(puzzle);
+                                possibilities[i, j] = null;
+                                hasChanged = true;
+                            }
+                            else
+                            {
+                                possibilities[i, j] = missingNUmbers;
+                            }
+                        }
+                        else
+                        {
+                            possibilities[i, j] = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Print(Sodoku puzzle, bool done = false)
+        {
+            string[] output = new string[9];
+
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    output[i] += puzzle.Get(i, j) + ", ";
+                }
+            }
+
+            foreach(string row in output)
+            {
+                Console.WriteLine(row);
+            }
+            Console.WriteLine("");
+
+            if (done)
+            {
+                Console.WriteLine("Done");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sodoku Solver/Sodoku.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sodoku_Solver
+{
+    class Sodoku
+    {
+        public int[,] board = new int[9, 9];
+
+        public Sodoku(int[,] startingBoard)
+        {
+            board = startingBoard;
+        }
+
+        public int Get(int i, int j)
+        {
+            return board[i,j];
+        }
+
+        public void Put(int i, int j, int value)
+        {
+            board[i, j] = value;
+        }
+
+        public List<int> GetNumbersMissingFromEveryDemension(int i, int j)
+        {
+            List<int> missingFromRow = GetMissingNumbers(GetRow(i));
+            List<int> missingFromColumn = GetMissingNumbers(GetColumn(j));
+            List<int> missingFromBlock = GetMissingNumbers(GetBlock(GetBlockNumber(i, j)));
+
+            List<int> missing = new List<int>();
+
+            for(int num = 1; num <= 9; num++)
+            {
+                if (missingFromRow.Contains(num) && missingFromColumn.Contains(num) && missingFromBlock.Contains(num))
+                {
+                    missing.Add(num);
+                }
+            }
+
+            return missing;
+        }
+
+        public int GetBlockNumber(int i, int j)
+        {
+            if(i < 3)
+            {
+                if (j < 3)
+                {
+                    return 0;
+                }
+                else if (j < 6)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else if(i < 6)
+            {
+                if (j < 3)
+                {
+                    return 3;
+                }
+                else if (j < 6)
+                {
+                    return 4;
+                }
+                else
+                {
+                    return 5;
+                }
+            }
+            else
+            {
+                if (j < 3)
+                {
+                    return 6;
+                }
+                else if (j < 6)
+                {
+                    return 7;
+                }
+                else
+                {
+                    return 8;
+                }
+            }
+        }
+
+        public List<int> GetMissingNumbers(int[] _input)
+        {
+            List<int> input = _input.ToList();
+            List<int> missingNumbers = new List<int>();
+
+            for(int i = 1; i <= 9; i++)
+            {
+                if (!input.Contains(i))
+                {
+                    missingNumbers.Add(i);
+                }
+            }
+
+            return missingNumbers;
+        }
+
+        public int[] GetRow(int rowNumber)
+        {
+            return Enumerable.Range(0, 9)
+                    .Select(x => board[rowNumber, x])
+                    .ToArray();
+        }
+
+        public int[] GetColumn(int columnNumber)
+        {
+            return Enumerable.Range(0, 9)
+                    .Select(x => board[x, columnNumber])
+                    .ToArray();
+        }
+
+        public int[] GetBlock(int blockNumber)
+        {
+            int[] block = new int[9];
+
+            if(blockNumber == 0)
+            {
+                block[0] = board[0, 0];
+                block[1] = board[0, 1];
+                block[2] = board[0, 2];
+                block[3] = board[1, 0];
+                block[4] = board[1, 1];
+                block[5] = board[1, 2];
+                block[6] = board[2, 0];
+                block[7] = board[2, 1];
+                block[8] = board[2, 2];
+            }
+            else if (blockNumber == 1)
+            {
+                block[0] = board[0, 3];
+                block[1] = board[0, 4];
+                block[2] = board[0, 5];
+                block[3] = board[1, 3];
+                block[4] = board[1, 4];
+                block[5] = board[1, 5];
+                block[6] = board[2, 3];
+                block[7] = board[2, 4];
+                block[8] = board[2, 5];
+            }
+            else if (blockNumber == 2)
+            {
+                block[0] = board[0, 6];
+                block[1] = board[0, 7];
+                block[2] = board[0, 8];
+                block[3] = board[1, 6];
+                block[4] = board[1, 7];
+                block[5] = board[1, 8];
+                block[6] = board[2, 6];
+                block[7] = board[2, 7];
+                block[8] = board[2, 8];
+            }
+
+            else if (blockNumber == 3)
+            {
+                block[0] = board[3, 0];
+                block[1] = board[3, 1];
+                block[2] = board[3, 2];
+                block[3] = board[4, 0];
+                block[4] = board[4, 1];
+                block[5] = board[4, 2];
+                block[6] = board[5, 0];
+                block[7] = board[5, 1];
+                block[8] = board[5, 2];
+            }
+            else if (blockNumber == 4)
+            {
+                block[0] = board[3, 3];
+                block[1] = board[3, 4];
+                block[2] = board[3, 5];
+                block[3] = board[4, 3];
+                block[4] = board[4, 4];
+                block[5] = board[4, 5];
+                block[6] = board[5, 3];
+                block[7] = board[5, 4];
+                block[8] = board[5, 5];
+            }
+            else if (blockNumber == 5)
+            {
+                block[0] = board[3, 6];
+                block[1] = board[3, 7];
+                block[2] = board[3, 8];
+                block[3] = board[4, 6];
+                block[4] = board[4, 7];
+                block[5] = board[4, 8];
+                block[6] = board[5, 6];
+                block[7] = board[5, 7];
+                block[8] = board[5, 8];
+            }
+
+            else if (blockNumber == 6)
+            {
+                block[0] = board[6, 0];
+                block[1] = board[6, 1];
+                block[2] = board[6, 2];
+                block[3] = board[7, 0];
+                block[4] = board[7, 1];
+                block[5] = board[7, 2];
+                block[6] = board[8, 0];
+                block[7] = board[8, 1];
+                block[8] = board[8, 2];
+            }
+            else if (blockNumber == 7)
+            {
+                block[0] = board[6, 3];
+                block[1] = board[6, 4];
+                block[2] = board[6, 5];
+                block[3] = board[7, 3];
+                block[4] = board[7, 4];
+                block[5] = board[7, 5];
+                block[6] = board[8, 3];
+                block[7] = board[8, 4];
+                block[8] = board[8, 5];
+            }
+            else if (blockNumber == 8)
+            {
+                block[0] = board[6, 6];
+                block[1] = board[6, 7];
+                block[2] = board[6, 8];
+                block[3] = board[7, 6];
+                block[4] = board[7, 7];
+                block[5] = board[7, 8];
+                block[6] = board[8, 6];
+                block[7] = board[8, 7];
+                block[8] = board[8, 8];
+            }
+
+            return block;
+        }
+    }
+}
